a40aa1cbfbc0dd1cc221fa1a106426b550026335
anshus7007 <60043003+anshus7007@users.noreply.github.com>
2020-11-02 01:21:14 +0530

    final submission

--- app/src/main/java/com/anshu/paytmui/MainActivity.java
+++ app/src/main/java/com/anshu/paytmui/MainActivity.java
@@ -1,14 +1,72 @@
 package com.anshu.paytmui;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+
+        Toolbar toolbar =findViewById(R.id.toolbar);
+        toolbar.setLogo(R.drawable.searchbar);
+        drawer=findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener= new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectFragment =null;
+            switch (item.getItemId())
+            {
+                case R.id.nav_home:
+                    selectFragment=new HomeFragment();
+                    break;
+                case R.id.nav_mall:
+                    selectFragment=new MallFragment();
+                    break;
+                case R.id.nav_bank:
+                    selectFragment= new BlankFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectFragment).commit();
+            return true;
+        }
+
+    };
+
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START))
+        {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+
     }
 }
